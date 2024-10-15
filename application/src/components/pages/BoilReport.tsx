c19274e7ea2fcb18ff0cf43bd310c7ca97583cdd
ShavedHedgehoc <48063136+ShavedHedgehoc@users.noreply.github.com>
2024-10-16 01:18:15 +0300

    fix boil report

--- application/src/components/pages/BoilReport.tsx
+++ application/src/components/pages/BoilReport.tsx
@@ -1,19 +1,33 @@
 import * as React from "react";
-import BreadCrumbHeader from "../headers/BreadCrumbHeader";
 import { Context } from "../../main";
-import MainPageHeaderWithRenewProp from "../headers/MainPageHeaderWithRenewProp";
+import { BoilFilterParams } from "../../store/BoilStore";
+import BreadCrumbHeader from "../headers/BreadCrumbHeader";
+import MainPageHeader from "../headers/MainPageHeader";
 import BoilsListTable from "../tables/boils_list_table/BoilsListTable";
 
 export default function BoilsReport() {
   const [initial, setInitial] = React.useState(false);
   const { store } = React.useContext(Context);
+
   React.useEffect(() => {
-    store.BoilStore.fetchBoils().then(() => setInitial(true));
-  });
+    store.HistoryTypeStore.fetchHistoryTypes()
+      .then(() => store.HistoryTypeStore.fetchHistoryTypesForBases())
+      .then(() => store.BoilStore.clearFilter())
+      .then(() =>
+        store.BoilStore.changeFilter({
+          key: BoilFilterParams.STATES,
+          value: "",
+          values: store.HistoryTypeStore.historyTypes.map((item) => item.id),
+        })
+      )
+      .then(() => setInitial(true))
+      .then(() => store.BoilStore.fetchBoils());
+  }, []);
+
   return (
     <React.Fragment>
       <BreadCrumbHeader breadcrumbs={["Отчеты", "Основы"]} />
-      <MainPageHeaderWithRenewProp title={"Основы"} renewData={() => store.BoilStore.fetchBoils()} />
+      <MainPageHeader pageTitle={"Основы"} />
       {initial && <BoilsListTable role={"reports"} />}
     </React.Fragment>
   );
--- application/src/components/pages/BoilsList.tsx
+++ application/src/components/pages/BoilsList.tsx
@@ -1,21 +1,25 @@
 import * as React from "react";
-import BreadCrumbHeader from "../headers/BreadCrumbHeader";
 import { Context } from "../../main";
-import BoilsListTable from "../tables/boils_list_table/BoilsListTable";
-import MainPageHeader from "../headers/MainPageHeader";
 import { BoilFilterParams } from "../../store/BoilStore";
+import BreadCrumbHeader from "../headers/BreadCrumbHeader";
+import MainPageHeader from "../headers/MainPageHeader";
+import BoilsListTable from "../tables/boils_list_table/BoilsListTable";
 
 export default function BoilsList() {
   const [initial, setInitial] = React.useState(false);
-
   const { store } = React.useContext(Context);
 
   React.useEffect(() => {
-    store.BoilStore.fetchBoils()
-      .then(() => store.HistoryTypeStore.fetchHistoryTypes())
+    store.HistoryTypeStore.fetchHistoryTypes()
       .then(() => store.HistoryTypeStore.fetchHistoryTypesForBases())
       .then(() => store.BoilStore.clearFilter())
-      .then(() => store.BoilStore.changeFilter({ key: BoilFilterParams.STATES, value: "", values: [1] }))
+      .then(() =>
+        store.BoilStore.changeFilter({
+          key: BoilFilterParams.STATES,
+          value: "",
+          values: [store.HistoryTypeStore.historyTypes[0].id],
+        })
+      )
       .then(() => setInitial(true))
       .then(() => store.BoilStore.fetchBoils());
   }, []);
