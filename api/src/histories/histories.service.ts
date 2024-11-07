ef28a8c97888e97c5f1aebd984ae453f26e4f616
ShavedHedgehoc <48063136+ShavedHedgehoc@users.noreply.github.com>
2024-11-07 20:01:05 +0300

    remove clg

--- api/src/histories/histories.service.ts
+++ api/src/histories/histories.service.ts
@@ -105,7 +105,6 @@ export class HistoriesService {
   }
 
   async addHistorie(dto: AddHistoryDtoNew) {
-    console.log(dto);
     const findRecordId = async () => {
       if (dto.boil_value && dto.code) {
         const record = await this.recordsService.getCurrentRecordByBoilAndCode(dto.boil_value, dto.code);
@@ -164,7 +163,6 @@ export class HistoriesService {
         lastHistory.historyType.value !== "plug_pass" &&
         lastHistory.historyType.value !== "product_fail"
       ) {
-        console.log(lastHistory.historyType.value);
         if (lastHistory.historyType.value === "product_check") {
           throw new HttpException("Продукт уже отнесен на пробу", HttpStatus.BAD_REQUEST);
         } else {
@@ -382,159 +380,4 @@ export class HistoriesService {
     });
     return histories;
   }
-
-  // async addHistoriesNew(dto: AddHistoriesDto) {
-  //   const historyType = await this.historyTypesService.getByValue(dto.historyType);
-  //   if (!historyType) {
-  //     throw new HttpException("Тип записи не найден", HttpStatus.NOT_FOUND);
-  //   }
-  //   if (dto.historyType === "base_check") {
-  //     const boil = await this.boilService.getOrCreateByValue(dto.boil);
-  //     const recDto = {
-  //       ...dto,
-  //       boilId: boil.id,
-  //       historyTypeId: historyType.id,
-  //     };
-  //     const history = await this.historyRepository.create(recDto);
-  //     await this.sendMessages([history]);
-  //     return history;
-  //   }
-  // }
-
-  // async addHistoriesToRecords(dto: AddHistoriesDto) {
-  //   const records = dto.code
-  //     ? await this.recordsService.getCurrentRecordsByBoilAndcode(dto.boil, dto.code)
-  //     : await this.recordsService.getCurrentRecordsByBoil(dto.boil);
-  //   if (records.length === 0) {
-  //     throw new HttpException("Строк сводки не найдено", HttpStatus.NOT_FOUND);
-  //   }
-
-  //   const historyType = await this.historyTypesService.getByValue(dto.historyType);
-  //   if (!historyType) {
-  //     throw new HttpException("Тип записи не найден", HttpStatus.NOT_FOUND);
-  //   }
-  //   //Move to userService
-  //   let userRoles = [];
-  //   if (dto.userId) {
-  //     const user = await this.userService.getByPk(dto.userId);
-  //     userRoles = user.roles ? user.roles.map((x) => x.description) : [];
-  //   }
-  //   // console.log(userRoles);
-  //   const recDto = records.map((item) => ({ ...dto, record_id: item.id, historyTypeId: historyType.id }));
-
-  //   let histories: History[] = [];
-  //   for (let index = 0; index < recDto.length; index++) {
-  //     const lastHistory = await this.getLastHistoryByRecId(recDto[index].record_id);
-  //     const lastHistoryType = lastHistory !== null ? lastHistory.historyType.value : null;
-
-  //     switch (recDto[index].historyType) {
-  //       case "base_check":
-  //         if (lastHistoryType !== null && lastHistoryType !== "base_fail" && userRoles.indexOf(DbRoles.GODMODE) === -1)
-  //           throw new HttpException(
-  //             `Чтобы запись могла быть внесена, запись должна быть первой или статус сводки должен быть "Брак основы"...`,
-  //             HttpStatus.BAD_REQUEST
-  //           );
-  //         break;
-  //       case "base_fail":
-  //         if (userRoles.indexOf(DbRoles.LABORATORY) === -1) {
-  //           throw new HttpException(
-  //             `Вносить запись этого типа могут только сотрудники лаборатории...`,
-  //             HttpStatus.BAD_REQUEST
-  //           );
-  //         }
-  //         break;
-  //       case "plug_pass":
-  //         // Лаборатория если основа на пробе или главный технолог если пусто
-  //         if (userRoles.indexOf(DbRoles.LABORATORY) === -1 && userRoles.indexOf(DbRoles.TECHNOLOGIST) === -1) {
-  //           throw new HttpException(
-  //             `Вносить запись этого типа могут только сотрудники лаборатории или главный технолог...`,
-  //             HttpStatus.BAD_REQUEST
-  //           );
-  //         }
-
-  //         if (
-  //           userRoles.indexOf(DbRoles.LABORATORY) !== -1 &&
-  //           userRoles.indexOf(DbRoles.TECHNOLOGIST) === -1 &&
-  //           lastHistoryType !== "base_check"
-  //         ) {
-  //           throw new HttpException(
-  //             `Чтобы запись могла быть внесена, статус сводки должен быть "Основа на пробе"...`,
-  //             HttpStatus.BAD_REQUEST
-  //           );
-  //         }
-
-  //         if (
-  //           userRoles.indexOf(DbRoles.LABORATORY) === -1 &&
-  //           userRoles.indexOf(DbRoles.TECHNOLOGIST) !== -1 &&
-  //           lastHistoryType !== null
-  //         ) {
-  //           throw new HttpException(
-  //             `Чтобы запись могла быть внесена, запись должна быть первой...`,
-  //             HttpStatus.BAD_REQUEST
-  //           );
-  //         }
-
-  //         if (
-  //           userRoles.indexOf(DbRoles.LABORATORY) !== -1 &&
-  //           userRoles.indexOf(DbRoles.TECHNOLOGIST) !== -1 &&
-  //           !(lastHistoryType === null || lastHistoryType === "base_check")
-  //         ) {
-  //           throw new HttpException(
-  //             `Чтобы запись могла быть внесена, запись должна быть первой или статус сводки должен быть "Основа на пробе"...`,
-  //             HttpStatus.BAD_REQUEST
-  //           );
-  //         }
-
-  //         break;
-  //       case "product_fail":
-  //         // Только лаборатория в любых
-  //         if (userRoles.indexOf(DbRoles.LABORATORY) === -1) {
-  //           throw new HttpException(
-  //             `Вносить запись этого типа могут только сотрудники лаборатории...`,
-  //             HttpStatus.BAD_REQUEST
-  //           );
-  //         }
-  //         break;
-  //       case "product_check":
-  //         // Только если есть допуск на подключение
-  //         if (lastHistoryType !== "plug_pass")
-  //           throw new HttpException(
-  //             `Чтобы запись могла быть внесена, статус сводки должен быть "Допуск на подключение"...`,
-  //             HttpStatus.BAD_REQUEST
-  //           );
-  //         break;
-  //       case "product_pass":
-  //         // Только лаборатория если продукт на пробе или проблемы с продуктом
-  //         if (userRoles.indexOf(DbRoles.LABORATORY) === -1) {
-  //           throw new HttpException(
-  //             `Вносить запись этого типа могут только сотрудники лаборатории...`,
-  //             HttpStatus.BAD_REQUEST
-  //           );
-  //         }
-  //         if (
-  //           userRoles.indexOf(DbRoles.LABORATORY) !== -1 &&
-  //           !(lastHistoryType === "product_fail" || lastHistoryType === "product_check")
-  //         ) {
-  //           throw new HttpException(
-  //             `Вносить запись этого типа могут только сотрудники лаборатории...`,
-  //             HttpStatus.BAD_REQUEST
-  //           );
-  //         }
-  //         break;
-  //       case "product_in_progress":
-  //         // Только мастер если есть допуск на фасовку
-  //         break;
-  //       case "product_finshed":
-  //         // Только мастер если продукт фасуется
-  //         break;
-  //       default:
-  //         break;
-  //     }
-
-  //     const history = await this.historyRepository.create(recDto[index]);
-  //     histories.push(history);
-  //   }
-  //   await this.sendMessages(histories);
-  //   return histories;
-  // }
 }
