3cdb4649e42ed074e9cf09d5fd0f5f9aaf853f35
ShavedHedgehoc <48063136+ShavedHedgehoc@users.noreply.github.com>
2025-02-04 02:32:02 +0300

    fix dash error

--- dash/src/modules/record/record-detail.tsx
+++ dash/src/modules/record/record-detail.tsx
@@ -118,106 +118,108 @@ export default function RecordDetail() {
                 </div>
               </div>
             )}
-            <div className="flex flex-row gap-3 ">
-              <div className="flex flex-col  w-3/4 flex-grow gap-3">
-                {(data.regulation.water_base_min_weight !== "NaN" ||
-                  data.regulation.water_base_max_weight !== "NaN") && (
-                  <div className="flex flex-row flex-grow gap-3">
-                    {data.regulation.water_base_min_weight !== "NaN" && (
-                      <div className=" flex flex-grow w-1/2 p-4 flex-col justify-between rounded-md bg-sky-700 text-slate-200">
-                        <div className="flex justify-start text-3xl ">Мин. вес</div>
-                        <div className="flex justify-end text-5xl">
-                          {data.regulation.org_base_min_weight !== "NaN"
-                            ? `${data.regulation.org_base_max_weight} + ${data.regulation.water_base_min_weight}`
-                            : data.regulation.water_base_min_weight}
+            {data.regulation && (
+              <div className="flex flex-row gap-3 ">
+                <div className="flex flex-col  w-3/4 flex-grow gap-3">
+                  {(data.regulation.water_base_min_weight !== "NaN" ||
+                    data.regulation.water_base_max_weight !== "NaN") && (
+                    <div className="flex flex-row flex-grow gap-3">
+                      {data.regulation.water_base_min_weight !== "NaN" && (
+                        <div className=" flex flex-grow w-1/2 p-4 flex-col justify-between rounded-md bg-sky-700 text-slate-200">
+                          <div className="flex justify-start text-3xl ">Мин. вес</div>
+                          <div className="flex justify-end text-5xl">
+                            {data.regulation.org_base_min_weight !== "NaN"
+                              ? `${data.regulation.org_base_max_weight} + ${data.regulation.water_base_min_weight}`
+                              : data.regulation.water_base_min_weight}
+                          </div>
                         </div>
-                      </div>
-                    )}
-                    {data.regulation.water_base_max_weight !== "NaN" && (
-                      <div className=" flex  flex-grow w-1/2 p-4 flex-col justify-between rounded-md bg-green-700 text-slate-200">
-                        <div className="flex justify-start text-3xl ">Макс. вес</div>
-                        <div className="flex justify-end text-5xl">
-                          {data.regulation.org_base_max_weight !== "NaN"
-                            ? `${data.regulation.org_base_max_weight} + ${data.regulation.water_base_min_weight}`
-                            : data.regulation.water_base_min_weight}
+                      )}
+                      {data.regulation.water_base_max_weight !== "NaN" && (
+                        <div className=" flex  flex-grow w-1/2 p-4 flex-col justify-between rounded-md bg-green-700 text-slate-200">
+                          <div className="flex justify-start text-3xl ">Макс. вес</div>
+                          <div className="flex justify-end text-5xl">
+                            {data.regulation.org_base_max_weight !== "NaN"
+                              ? `${data.regulation.org_base_max_weight} + ${data.regulation.water_base_min_weight}`
+                              : data.regulation.water_base_min_weight}
+                          </div>
                         </div>
-                      </div>
-                    )}
-                  </div>
-                )}
-                {(data.regulation.per_box !== 0 ||
-                  data.regulation.box_per_row !== 0 ||
-                  data.regulation.row_on_pallet !== 0) && (
-                  <div className="flex flex-row flex-grow gap-3">
-                    {data.regulation.per_box !== 0 && (
-                      <div className=" flex  flex-grow w-1/3 p-4 flex-col justify-between rounded-md bg-lime-700 text-slate-200">
-                        <div className="flex justify-start text-3xl ">В коробе</div>
-                        <div className="flex justify-end text-5xl">{data.regulation.per_box}</div>
-                      </div>
-                    )}
-                    {data.regulation.box_per_row !== 0 && (
-                      <div className=" flex  flex-grow w-1/3 p-4 flex-col justify-between rounded-md bg-pink-700 text-slate-200">
-                        <div className="flex justify-start text-3xl ">Коробов в ряду</div>
-                        <div className="flex justify-end text-5xl">{data.regulation.box_per_row}</div>
-                      </div>
-                    )}
-                    {data.regulation.row_on_pallet !== 0 && (
-                      <div className=" flex flex-grow w-1/3 p-4 flex-col justify-between rounded-md bg-yellow-700 text-slate-200">
-                        <div className="flex justify-start text-3xl ">Рядов на паллете</div>
-                        <div className="flex justify-end text-5xl">{data.regulation.row_on_pallet}</div>
-                      </div>
-                    )}
-                  </div>
-                )}
-                {(data.regulation.gasket || data.regulation.seal) && (
-                  <div className="flex flex-row flex-grow gap-3">
-                    {data.regulation.gasket && (
-                      <div className=" flex flex-grow w-1/2 p-4 flex-col  gap-3 rounded-md bg-amber-700 text-slate-200">
-                        <div className="flex justify-start text-3xl ">Прокладка:</div>
-                        <div className="flex justify-left items-center text-2xl ">
-                          {data.regulation.gasket ? data.regulation.gasket : " "}
-                        </div>
-                      </div>
-                    )}
-                    {data.regulation.seal && (
-                      <div className=" flex w-1/2 p-4 flex-col justify-center  animate-pulse rounded-md bg-cyan-700 text-slate-200">
-                        <div className="flex justify-center items-center text-4xl ">Не запечатываем</div>
-                      </div>
-                    )}
-                  </div>
-                )}
-                {data.regulation.technician_note && (
-                  <div className=" flex w-full p-4 flex-col  flex-grow gap-3 justify-start rounded-md bg-green-700 text-slate-200">
-                    <div className="flex justify-start text-3xl ">Примечания для техников:</div>
-                    <div className="flex justify-start text-2xl">
-                      {data.regulation.technician_note ? data.regulation.technician_note : " "}
+                      )}
                     </div>
-                  </div>
-                )}
-                {data.regulation.packaging_note && (
-                  <div className=" flex w-full p-4 flex-col  flex-grow gap-3 justify-start rounded-md bg-cyan-700 text-slate-200">
-                    <div className="flex justify-start text-3xl ">Примечания для фасовки:</div>
-                    <div className="flex justify-start text-2xl">
-                      {data.regulation.packaging_note ? data.regulation.packaging_note : " "}
+                  )}
+                  {(data.regulation.per_box !== 0 ||
+                    data.regulation.box_per_row !== 0 ||
+                    data.regulation.row_on_pallet !== 0) && (
+                    <div className="flex flex-row flex-grow gap-3">
+                      {data.regulation.per_box !== 0 && (
+                        <div className=" flex  flex-grow w-1/3 p-4 flex-col justify-between rounded-md bg-lime-700 text-slate-200">
+                          <div className="flex justify-start text-3xl ">В коробе</div>
+                          <div className="flex justify-end text-5xl">{data.regulation.per_box}</div>
+                        </div>
+                      )}
+                      {data.regulation.box_per_row !== 0 && (
+                        <div className=" flex  flex-grow w-1/3 p-4 flex-col justify-between rounded-md bg-pink-700 text-slate-200">
+                          <div className="flex justify-start text-3xl ">Коробов в ряду</div>
+                          <div className="flex justify-end text-5xl">{data.regulation.box_per_row}</div>
+                        </div>
+                      )}
+                      {data.regulation.row_on_pallet !== 0 && (
+                        <div className=" flex flex-grow w-1/3 p-4 flex-col justify-between rounded-md bg-yellow-700 text-slate-200">
+                          <div className="flex justify-start text-3xl ">Рядов на паллете</div>
+                          <div className="flex justify-end text-5xl">{data.regulation.row_on_pallet}</div>
+                        </div>
+                      )}
+                    </div>
+                  )}
+                  {(data.regulation.gasket || data.regulation.seal) && (
+                    <div className="flex flex-row flex-grow gap-3">
+                      {data.regulation.gasket && (
+                        <div className=" flex flex-grow w-1/2 p-4 flex-col  gap-3 rounded-md bg-amber-700 text-slate-200">
+                          <div className="flex justify-start text-3xl ">Прокладка:</div>
+                          <div className="flex justify-left items-center text-2xl ">
+                            {data.regulation.gasket ? data.regulation.gasket : " "}
+                          </div>
+                        </div>
+                      )}
+                      {data.regulation.seal && (
+                        <div className=" flex w-1/2 p-4 flex-col justify-center  animate-pulse rounded-md bg-cyan-700 text-slate-200">
+                          <div className="flex justify-center items-center text-4xl ">Не запечатываем</div>
+                        </div>
+                      )}
+                    </div>
+                  )}
+                  {data.regulation.technician_note && (
+                    <div className=" flex w-full p-4 flex-col  flex-grow gap-3 justify-start rounded-md bg-green-700 text-slate-200">
+                      <div className="flex justify-start text-3xl ">Примечания для техников:</div>
+                      <div className="flex justify-start text-2xl">
+                        {data.regulation.technician_note ? data.regulation.technician_note : " "}
+                      </div>
+                    </div>
+                  )}
+                  {data.regulation.packaging_note && (
+                    <div className=" flex w-full p-4 flex-col  flex-grow gap-3 justify-start rounded-md bg-cyan-700 text-slate-200">
+                      <div className="flex justify-start text-3xl ">Примечания для фасовки:</div>
+                      <div className="flex justify-start text-2xl">
+                        {data.regulation.packaging_note ? data.regulation.packaging_note : " "}
+                      </div>
+                    </div>
+                  )}
+                </div>
+                {data.regulation.marking_sample_value && (
+                  <div className="flex w-1/4  flex-col  justify-between p-4  gap-4 rounded-md bg-orange-700 text-slate-200">
+                    <div className="flex justify-start text-3xl ">Маркировка</div>
+                    <div className="flex flex-col gap-2">
+                      <div className="flex justify-start text-2xl">Цвет чернил: {data.regulation.inc_color}</div>
+                      <div className="flex justify-start text-xl">{data.regulation.marking_feature}</div>
+                      <img
+                        className="object-fill rounded-md"
+                        alt={`http://ones-esb-vm:9000/manufacturing/templates/marking/${data.regulation.marking_sample_value}.jpg`}
+                        src={`http://ones-esb-vm:9000/manufacturing/templates/marking/${data.regulation.marking_sample_value}.jpg`}
+                      />
                     </div>
                   </div>
                 )}
               </div>
-              {data.regulation.marking_sample_value && (
-                <div className="flex w-1/4  flex-col  justify-between p-4  gap-4 rounded-md bg-orange-700 text-slate-200">
-                  <div className="flex justify-start text-3xl ">Маркировка</div>
-                  <div className="flex flex-col gap-2">
-                    <div className="flex justify-start text-2xl">Цвет чернил: {data.regulation.inc_color}</div>
-                    <div className="flex justify-start text-xl">{data.regulation.marking_feature}</div>
-                    <img
-                      className="object-fill rounded-md"
-                      alt={`http://ones-esb-vm:9000/manufacturing/templates/marking/${data.regulation.marking_sample_value}.jpg`}
-                      src={`http://ones-esb-vm:9000/manufacturing/templates/marking/${data.regulation.marking_sample_value}.jpg`}
-                    />
-                  </div>
-                </div>
-              )}
-            </div>
+            )}
           </div>
         )}
         {/* {isSuccess && <RegulationDetail productId={data?.productId} />} */}
